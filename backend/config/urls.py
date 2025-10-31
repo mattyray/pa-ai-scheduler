3e378448ac5904ba38f5d5e514922fefc30c3d0f
mattyray <mnraynor90@gmail.com>
2025-10-31 14:27:20 -0400

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.users.urls')),
+    path('api/', include('apps.schedules.urls')),  # ADD THIS LINE
 ]
