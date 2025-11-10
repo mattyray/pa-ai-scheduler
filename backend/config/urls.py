0514c14e73be5eb59f544214a5d509427c019dc6
mattyray <mnraynor90@gmail.com>
2025-11-09 22:00:26 -0500

    fly and urls set for dep

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({'status': 'healthy'})
 
 urlpatterns = [
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.users.urls')),
     path('api/schedule-periods/', include('apps.schedules.urls')),
