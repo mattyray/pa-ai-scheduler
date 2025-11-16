3a3c0c6cd2560e8a488282252c0c511e9d769563
mattyray <mnraynor90@gmail.com>
2025-11-16 15:44:23 -0500

    asgi

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -7,14 +7,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
 
-# Import after Django is set up
-from apps.schedules.routing import websocket_urlpatterns
+from apps.schedules.routing import websocket_urlpatterns as schedule_urlpatterns
+from apps.chat.routing import websocket_urlpatterns as chat_urlpatterns
+
+all_websocket_patterns = schedule_urlpatterns + chat_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            all_websocket_patterns
         )
     ),
 })
